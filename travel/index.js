085dec126cab917ef1157d88bdbfc3a791e6c4ab
BrBrov <epoc@list.ru>
2022-06-29 23:34:45 +0300

    feat: Fix markup with JS

--- travel/index.js
+++ travel/index.js
@@ -29,8 +29,20 @@ const blockMarksOfDots = {
     active: 'dot active-dot',
     inactive: 'dot inactive-dot'
 }
+const pathStory ={
+    cave: ['cave-big.jpg','cave.jpg'],
+    road: ['road-big.jpg', 'road.jpg'],
+    winter : ['winter-big.jpg', 'winter.jpg'],
+    car : ['car-big.jpg', 'car.jpg']
+};
+const textStory = {
+    desktop : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+    mobile : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla.Lorem ipsum dolor sit a...'
+}
 let carouselItemsImg = document.getElementsByClassName('img-item');
 let dots = document.getElementsByClassName('dot');
+let imgStory = document.getElementsByClassName('img-story');
+let textStoryDivision = document.getElementsByClassName('text-story');
 
 addEventListener('resize', adaptive);
 
@@ -38,15 +50,27 @@ function adaptive(){
     let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
     if(width<=390){
         carouselItemsImg[0].attributes[1].nodeValue = path + "ocean.jpg";
-        log(carouselItemsImg[0].attributes['src']);
         dots[1].className = blockMarksOfDots.inactive;
         dots[0].className = blockMarksOfDots.active;
-        log(dots[0].className);
+        imgStory[0].attributes[1].nodeValue = path + pathStory.cave[1];
+        imgStory[1].attributes[1].nodeValue = path + pathStory.road[1];
+        imgStory[2].attributes[1].nodeValue = path + pathStory.winter[1];
+        imgStory[3].attributes[1].nodeValue = path + pathStory.car[1];
+        for(let i=0; i<4;i++){
+            textStoryDivision[i].textContent = textStory.mobile;
+        }
         log('page was resized - Adaptive!')
     }else{
         carouselItemsImg[0].attributes[1].nodeValue = "./assets/jpg/ocean-big.jpg";
         dots[1].className = blockMarksOfDots.active;
         dots[0].className = blockMarksOfDots.inactive;
+        imgStory[0].attributes[1].nodeValue = path + pathStory.cave[0];
+        imgStory[1].attributes[1].nodeValue = path + pathStory.road[0];
+        imgStory[2].attributes[1].nodeValue = path + pathStory.winter[0];
+        imgStory[3].attributes[1].nodeValue = path + pathStory.car[0];
+        for(let i=0; i<4;i++){
+            textStoryDivision[i].textContent = textStory.desktop;
+        }
         log('no adaptive!');
     }
 }
