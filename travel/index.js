39604052a9658c822e9e6924b91ef9d9dcf977e1
BrBrov <epoc@list.ru>
2022-07-13 22:44:53 +0300

    refactor: Refactor JS code

--- travel/index.js
+++ travel/index.js
@@ -1,29 +1,20 @@
 let {log} = console;
 
-let requirements = [
-    'Вёрстка соответствует макету. Ширина экрана 390px +48',
-    'блок <header> +6',
-    'секция preview +9',
-    'секция steps +9',
-    'секция destinations +9',
-    'секция stories +9',
-    'блок <footer> +6',
-    'Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15',
-    'нет полосы прокрутки при ширине страницы от 1440рх до 390px +7',
-    'нет полосы прокрутки при ширине страницы от 390px до 320рх +8',
-    'На ширине экрана 390рх и меньше реализовано адаптивное меню +22',
-    'при ширине страницы 390рх панель навигации скрывается, появляется бургер-иконка +2',
-    'при нажатии на бургер-иконку плавно появляется адаптивное меню +4',
-    'адаптивное меню соответствует макету +4',
-    'при нажатии на крестик адаптивное меню плавно скрывается уезжая за экран +4',
-    'ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +4 (все кроме Account, она пока что просто закрывает меню)',
-    'при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, так же скрытие меню происходит если сделать клик вне данного окна +4',
-    'Total score 85\nScore 75'
-];
-
-requirements.forEach((value) => {
-    log(value);
-})
+// let requirements = [
+//     'Слайдер изображений в секции destinations +50',
+//     'на десктоп варианте при клике на урезанную картинку слева или справа изображение меняется по принципу карусели(например если нажать правую картинку та что была в центре на уезжает налево, а та что была видна наполовину оказывается справа) + 20',
+//     'Три точки внизу отображают "/номер слайда"/, то есть каждому слайду соответствует свой кружочек, который становится активным при нахождении соответствующего ему слайда в центре. На мобильном варианте картинка одна, но поверх нее появляются стрелочки навигации (можно сделать как карусель или же затемнять кнопку если слайдер достиг края) +20',
+//     'Анимации плавного перемещения для слайдера +10',
+//     'Нажатие на кнопку Login (кнопка Account в мобильной версии) показывает сверстанный логин попап + 50',
+//     'логин попап соответствует верстке его закрытие происходит при клике вне попапа +25',
+//     'логин попап имеет 2 инпута (логин и пароль) при нажатии на кнопку Sign In показывается браузерный алерт с введенными данными (для реализации можно использовать тег ) +25',
+//     'Нажатие на кнопку Register на Login попапе меняет разметку попапа на разметку Sign Up попапа согласно макету (То есть нажатие не закрывает модал а просто меняет его наполнение). +25',
+//     'Total score 85\nScore 75'
+// ];
+//
+// requirements.forEach((value) => {
+//     log(value);
+// })
 
 addEventListener('resize', adaptive);
 
@@ -62,6 +53,87 @@ function adaptive() {
         for (let i = 0; i < 4; i++) {
             textStoryDivision[i].textContent = textStory.mobile;
         }
+        let menuButton = document.querySelector('.btn-login');
+
+        menuButton.addEventListener('click', () => {
+
+            if (!document.querySelector('.burger-menu')) {
+
+                if (Math.max(document.documentElement.clientWidth, window.innerWidth || 0) <= 390) {
+
+
+                    let webPageElem = document.querySelector('.container');
+                    const links = ['How It Works', 'Destinations', 'Plan Your Trip', 'Travel Stories', 'Account', 'Social Media'];
+                    let divElem = document.createElement('div');
+                    divElem.className = 'burger-menu';
+                    divElem.style.animation = 'menu 1.4s alternate forwards';
+                    let closeElem = document.createElement('div');
+                    closeElem.className = 'x-close';
+                    let navElem = document.createElement('div');
+                    navElem.className = 'burger-menu-block';
+
+                    for (let c in links) {
+                        let linkElem = document.createElement('div');
+                        linkElem.className = 'burger-link';
+                        linkElem.textContent = links[c];
+                        navElem.appendChild(linkElem);
+                    }
+                    divElem.appendChild(closeElem);
+                    divElem.appendChild(navElem);
+                    webPageElem.append(divElem);
+
+                    let burgerButton = document.getElementsByClassName('burger-menu')[0];
+
+                    document.body.addEventListener('touchstart', () => {
+
+                        if (document.getElementsByClassName('burger-menu')[0]) {
+                            burgerButton.style.animation = 'menuClose 1.4s alternate forwards';
+                            setTimeout(() => {
+                                burgerButton.remove()
+                            }, 1450);
+                        }
+                    }, {capture: true})
+
+                    burgerButton.addEventListener('touchstart', (eventData) => {
+
+                        if (eventData.target.className === 'x-close') {
+                            document.getElementsByClassName('x-close')[0].style.transition = 'all 1s ease-in-out';
+                            document.getElementsByClassName('x-close')[0].style.transform = 'rotate(720deg)';
+                            burgerButton.style.animation = 'menuClose 1.4s alternate forwards';
+                            setTimeout(() => {
+                                burgerButton.remove()
+                            }, 1450);
+                        } else {
+                            switch (eventData.target.outerText) {
+                                case links[0]:
+                                    document.getElementsByClassName('burger-link')[0].style.color = 'grey';
+                                    document.getElementById('preview').scrollIntoView(false);
+                                    break;
+                                case links[1]:
+                                    document.getElementsByClassName('burger-link')[1].style.color = 'grey';
+                                    window.scrollTo(0, document.getElementById('steps').offsetTop);
+                                    break;
+                                case links[2]:
+                                    document.getElementsByClassName('burger-link')[2].style.color = 'grey';
+                                    window.scrollTo(0, document.getElementById('destinations').offsetTop);
+                                    break;
+                                case links[3]:
+                                    document.getElementsByClassName('burger-link')[3].style.color = 'grey';
+                                    window.scrollTo(0, document.getElementById('stories').offsetTop);
+                                    break;
+                                case links[4]:
+                                    document.getElementsByClassName('burger-link')[4].style.color = 'grey';
+                                    break;
+                                case links[5]:
+                                    document.getElementsByClassName('burger-link')[5].style.color = 'grey';
+                                    document.getElementsByClassName('social-list')[0].scrollIntoView();
+                                    break;
+                            }
+                        }
+                    })
+                }
+            }
+        });
     } else {
         carouselItemsImg[0].attributes[1].nodeValue = "./assets/jpg/ocean-big.jpg";
         dots[1].className = blockMarksOfDots.active;
@@ -76,87 +148,7 @@ function adaptive() {
     }
 }
 
-let menuButton = document.querySelector('.btn-login');
 
-menuButton.addEventListener('click', () => {
-
-    if (!document.querySelector('.burger-menu')) {
-
-        if (Math.max(document.documentElement.clientWidth, window.innerWidth || 0) <= 390) {
-
-
-            let webPageElem = document.querySelector('.container');
-            const links = ['How It Works', 'Destinations', 'Plan Your Trip', 'Travel Stories', 'Account', 'Social Media'];
-            let divElem = document.createElement('div');
-            divElem.className = 'burger-menu';
-            divElem.style.animation = 'menu 1.4s alternate forwards';
-            let closeElem = document.createElement('div');
-            closeElem.className = 'x-close';
-            let navElem = document.createElement('div');
-            navElem.className = 'burger-menu-block';
-
-            for (let c in links) {
-                let linkElem = document.createElement('div');
-                linkElem.className = 'burger-link';
-                linkElem.textContent = links[c];
-                navElem.appendChild(linkElem);
-            }
-            divElem.appendChild(closeElem);
-            divElem.appendChild(navElem);
-            webPageElem.append(divElem);
-
-            let burgerButton = document.getElementsByClassName('burger-menu')[0];
-
-            document.body.addEventListener('touchstart', () => {
-
-                if (document.getElementsByClassName('burger-menu')[0]) {
-                    burgerButton.style.animation = 'menuClose 1.4s alternate forwards';
-                    setTimeout(() => {
-                        burgerButton.remove()
-                    }, 1450);
-                }
-            }, {capture: true})
-
-            burgerButton.addEventListener('touchstart', (eventData) => {
-
-                if (eventData.target.className === 'x-close') {
-                    document.getElementsByClassName('x-close')[0].style.transition = 'all 1s ease-in-out';
-                    document.getElementsByClassName('x-close')[0].style.transform = 'rotate(720deg)';
-                    burgerButton.style.animation = 'menuClose 1.4s alternate forwards';
-                    setTimeout(() => {
-                        burgerButton.remove()
-                    }, 1450);
-                } else {
-                    switch (eventData.target.outerText) {
-                        case links[0]:
-                            document.getElementsByClassName('burger-link')[0].style.color = 'grey';
-                            document.getElementById('preview').scrollIntoView(false);
-                            break;
-                        case links[1]:
-                            document.getElementsByClassName('burger-link')[1].style.color = 'grey';
-                            window.scrollTo(0, document.getElementById('steps').offsetTop);
-                            break;
-                        case links[2]:
-                            document.getElementsByClassName('burger-link')[2].style.color = 'grey';
-                            window.scrollTo(0, document.getElementById('destinations').offsetTop);
-                            break;
-                        case links[3]:
-                            document.getElementsByClassName('burger-link')[3].style.color = 'grey';
-                            window.scrollTo(0, document.getElementById('stories').offsetTop);
-                            break;
-                        case links[4]:
-                            document.getElementsByClassName('burger-link')[4].style.color = 'grey';
-                            break;
-                        case links[5]:
-                            document.getElementsByClassName('burger-link')[5].style.color = 'grey';
-                            document.getElementsByClassName('social-list')[0].scrollIntoView();
-                            break;
-                    }
-                }
-            })
-        }
-    }
-});
 adaptive();
 
 
